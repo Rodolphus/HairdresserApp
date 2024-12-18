4dfba5233e47b108c1ee2ac172911a636dedda98
Rodolphus <mehmetakifturhan99@gmail.com>
2024-12-18 21:06:52 +0300

    Initial commit

---
+++ hairdresserApp/Data/HairdresserAppContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HairdresserApp.Data
+{
+    public class HairdresserAppContext : DbContext
+    {
+        public HairdresserAppContext(DbContextOptions<HairdresserAppContext> options) : base(options) { }
+
+
+    }
+}
